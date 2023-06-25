10fc67b1072f46a3dec667d46146430a5bf36fbd
AndresGutierrez <c0dingw.dre@gmail.com>
2023-06-25 14:43:16 -0700

    Initial Commit

---
+++ RealmSwiftUI/RealmSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmSwiftUIApp.swift
+//  RealmSwiftUI
+//
+//  Created by Andres Gutierrez on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RealmSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
